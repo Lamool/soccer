2ec6ac0887e71772db2a82c2ce6160c09de12d44
kimruafase <ppooppplus33@gmail.com>
2024-06-11 15:03:52 +0900

    2024-06-11

--- 03_mainPage.js
+++ 03_mainPage.js
@@ -7,6 +7,10 @@ const nextBtn = document.querySelector(".next");
 const images = document.querySelector(".images");
 
 let titleList = [];
+let rankList = [];
+let accTeam = { };
+let newTitleList = [];
+
 function next(){
     if(pages < 4){
         backBtn.removeAttribute(`disabled`);
@@ -35,28 +39,154 @@ function print(){
     nextBtn.addEventListener("click", next);
 }
 print();
+function pref(){
+    rankList = JSON.parse(localStorage.getItem(`memberList`));
 
+    for(let i = 0; i<rankList.length ; i++){
+
+        let m = rankList[i];
+
+        if( accTeam.hasOwnProperty(`${m.soccer}`) ){
+            accTeam[`${m.soccer}`] = accTeam[`${m.soccer}`] +1
+        }else{
+            accTeam[`${m.soccer}`] = 1;
+        }
+    }
+    console.log( accTeam );
+}
 function boardPrint(){
-    let board = document.querySelector(`.CB2`);
+    let board = document.querySelector(`#CB2`);
+    let clubRank = document.querySelector(`#CB3`);
     titleList = JSON.parse(localStorage.getItem(`boardList`));
+    rankList = JSON.parse(localStorage.getItem(`memberList`));
     if(titleList == null){return;};
+    if(rankList == null){return;};
+
+    for(i=titleList.length-1; i>-1; i--){
+        newTitleList.push(titleList[i]);
+    }
+    console.log(newTitleList);
 
     let html = ``;
-    for(i=0; i<titleList.length; i++){
+    let html1 = ``;
+    let asd = newTitleList.length <= 6 ? newTitleList.length : 6 
+    for(i=0; i<asd ; i++){
         html +=`
                 <li>
-                    <span id="CBT">
-                        ${i+1}
+                    <span class="CBT">
+                        ${newTitleList[i].no}
                     </span>
                     <a href="#">
-                        ${titleList[i].title}
+                        ${newTitleList[i].title}
                     </a>
-                    <span id="CBV">
-                        [${titleList[i].view}]
+                    <span class="CBV">
+                        [${newTitleList[i].view}]
                     </span>
                 </li>
                 `;
     }
+    pref();
+    let length = Object.keys(accTeam).length;
+    let sort = Object.fromEntries(
+        Object.entries(accTeam).sort(([, a], [, b]) => b - a)
+    );
+    let arr = Object.keys(sort);
+    let value = Object.values(sort);
+    console.log(sort);
+    for(j=0; j<length; j++){
+        html1 += `
+                <li>
+                    <span class="CBT">
+                        ${j+1}
+                    </span>
+                    <a href="#">
+                        ${arr[j]}
+                    </a>
+                    <span class="CBV">
+                        [${value[j]}]
+                    </span>
+                </li>`;    
+    }
+    console.log(html1);
     board.innerHTML = html;
+    clubRank.innerHTML = html1;
+}
+function loginState(){
+    let log = document.querySelector(`#HT`);
+    let html = ``;
+    let loginNo = sessionStorage.getItem('loginNo');
+    if( loginNo == null ){ loginNo = 0 };
+    if( loginNo != 0 ){
+        html =`<li>
+                    <a href="#" onclick="logOut()">
+                        <span>
+                            로그아웃
+                        </span>
+                    </a>
+                    |
+                </li>
+                <li>
+                    <a href="signup.html">
+                        <span>
+                            회원가입
+                        </span>
+                    </a>
+                    |
+                </li>
+                <li>
+                    <a href="#">
+                        <span>
+                            비밀번호찾기
+                        </span>
+                    </a>
+                    |
+                </li>
+                <li>
+                    <a href="#">
+                        <span>
+                            모바일앱
+                        </span>
+                    </a>
+                </li>`;
+    }else{
+        html = `<li>
+                    <a href="login.html">
+                        <span>
+                            로그인
+                        </span>
+                    </a>
+                    |
+                </li>
+                <li>
+                    <a href="signup.html">
+                        <span>
+                            회원가입
+                        </span>
+                    </a>
+                    |
+                </li>
+                <li>
+                    <a href="#">
+                        <span>
+                            비밀번호찾기
+                        </span>
+                    </a>
+                    |
+                </li>
+                <li>
+                    <a href="#">
+                        <span>
+                            모바일앱
+                        </span>
+                    </a>
+                </li>`;
+    }
+    log.innerHTML = html;
+}
+function logOut(){
+    sessionStorage.removeItem('loginNo');
+    alert('로그아웃 되었습니다.');
+    location.href=`03_mainPage.html`;
 }
 boardPrint();
+loginState();

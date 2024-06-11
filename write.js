f19c6a7961febff3f0720c031c905b3204166834
dlwoauddlqslek <ljm5857@gmail.com>
2024-06-11 15:47:17 +0900

    2024-06-11

--- write.js
+++ write.js
@@ -72,3 +72,74 @@ function modify(){
 function goboard(){
   location.href="soccerboard.html"
 }
+function loginState(){
+  let log = document.querySelector(`#HT`);
+  let html = ``;
+  let loginNo = sessionStorage.getItem('loginNo');
+  if( loginNo == null ){ loginNo = 0 };
+  if( loginNo != 0 ){
+      html =`<li>
+                  <a href="#" onclick="logOut()">
+                      <span>
+                          로그아웃
+                      </span>
+                  </a>
+                  |
+              </li>
+              <li>
+                  <a href="info.html">
+                      <span>
+                          내정보
+                      </span>
+                  </a>
+                  |
+              </li>
+              
+              <li>
+                  <a href="#">
+                      <span>
+                          모바일앱
+                      </span>
+                  </a>
+              </li>`;
+  }else{
+      html = `<li>
+                  <a href="login.html">
+                      <span>
+                          로그인
+                      </span>
+                  </a>
+                  |
+              </li>
+              <li>
+                  <a href="signup.html">
+                      <span>
+                          회원가입
+                      </span>
+                  </a>
+                  |
+              </li>
+              <li>
+                  <a href="#">
+                      <span>
+                          비밀번호찾기
+                      </span>
+                  </a>
+                  |
+              </li>
+              <li>
+                  <a href="#">
+                      <span>
+                          모바일앱
+                      </span>
+                  </a>
+              </li>`;
+  }
+  log.innerHTML = html;
+}
+function logOut(){
+  sessionStorage.removeItem('loginNo');
+  alert('로그아웃 되었습니다.');
+  location.href=`03_mainPage.html`;
+}
+loginState()

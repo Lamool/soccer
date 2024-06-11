a0d35b3d8f793bf2bb63cf571c10028fab74c567
tjwlsvy <tjwlsvy1@naver.com>
2024-06-11 10:21:56 +0900

    iogin

--- login.js
+++ login.js
@@ -16,7 +16,7 @@ if(memberList == null){memberList = [];}
     if(memberList[i].id == id && memberList[i].pw == pw){
       alert('로그인성공');
 
-      sessionStorage.setItem('loginNo' , menubar.no);
+      sessionStorage.setItem('loginNo' , memberList[i].no);
 
       location.href = '';
       return;
---
+++ update.js
@@ -0,0 +1,10 @@
+
+
+let memberList = [];
+
+function update(){
+
+  let pw = document.querySelector('#pw').value
+  let newPw = document.querySelector('#newPw').value
+  let newPwConfirm = document.querySelector('#newPwConfirm').value
+}
